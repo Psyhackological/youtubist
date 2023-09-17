6bd415bf25bd1e906f7cf5c20c641ae6141fadd9
Psyhackological <konradkon@duck.com>
2023-09-17 13:07:03 +0200

    Fixed issue with entries that now has 2 (0 is Videos, 1 is Shorts)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 import pyperclip
 import yt_dlp
 
@@ -6,23 +8,28 @@ from argparse_parsing import parse_arguments
 
 def fetch_youtube_playlist_info(playlist_url):
     ydl_opts = {
-        'quiet': True,
-        'extract_flat': True,
-        'force_generic_extractor': True,
+        "extract_flat": True,
+        "force_generic_extractor": True,
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(playlist_url, download=False)
-        entries = info_dict.get('entries', [])
+
+        # Write the debug information to a JSON file
+        with open("debug_info.json", "w", encoding="utf-8") as json_file_handle:
+            json.dump(info_dict, json_file_handle, indent=4)
+
+        # Directly access the entries under "Videos", which is assumed to be the first entry
+        videos_info = info_dict.get("entries", [{}])[0].get("entries", [])
 
     formatted_output = []
-    for video_info in entries:
+    for video_info in videos_info:
         title = video_info.get("title", "Unknown Title")
-        url = video_info.get("url", "Unknown URL")
-        duration = video_info.get("duration", "Unknown Duration")
-
-        duration_hms = f"{int(duration // 3600):02d}:{int((duration % 3600) // 60):02d}:{int(duration % 60):02d}"
-
+        url = video_info.get("id", "Unknown URL")
+        duration = int(video_info.get("duration", 0))
+        duration_hms = (
+            f"{duration // 3600:02d}:{(duration % 3600) // 60:02d}:{duration % 60:02d}"
+        )
         formatted_output.append(
             f"[{title}](https://youtu.be/{url}) | {duration_hms}")
 
@@ -34,7 +41,6 @@ def main():
     formatted_info = fetch_youtube_playlist_info(args.url)
     print("Formatted Video Information:")
     print(formatted_info)
-
     pyperclip.copy(formatted_info)
     print("Formatted video information has been copied to clipboard.")
 
